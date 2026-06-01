233def9b412c167ec5ba0afdd9c8818b49ee2910
Micky <mikiyasseifu12@gmail.com>
2026-06-01 12:34:21 +0300

    add CORS support

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///problems.db'
 db = SQLAlchemy(app)
 
